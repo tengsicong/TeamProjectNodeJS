c6412cfcd41ddcd5bc30925b61179c47916b537a
Sicong Teng <steng2@sheffield.ac.uk>
2020-04-01 18:04:03 +0100

    frame of TeamProject

--- routes/student.js
+++ routes/student.js
@@ -6,33 +6,28 @@ const UsersModel = require('../models/student');
 // UsersModel.getStudentByID(studentID).then(function(students) {
 //     console.log(students['Name']);
 // });
-// const config = require('config-lite')(__dirname)
-// const Mongolass = require('mongolass')
-// const mongolass = new Mongolass()
-// mongolass.connect(config.mongodb, {dbName: 'TeamProject'})
+const config = require('config-lite')(__dirname)
+const Mongolass = require('mongolass')
+const mongolass = new Mongolass()
+mongolass.connect(config.mongodb, {dbName: 'TeamProject'})
 // const students = mongolass.model('students')
-// // const students = require('../lib/mongo').students
-// students
-//     .find()
-//     .exec()
-//     .then(console.log)
-//     .catch(console.error)
-//
-// const Student = mongolass.model('students', {
-//     StudentID: {type: 'Number', required: true},
-//     UserName: {type: 'string', required: true},
-//     Password: {type: 'string', required: true},
-//     Name: {type: 'string', required: true},
-//     GroupID: {type: 'Number'},
-//     PeoplePreference: [Number],
-//     Mark: [Number],
-// });
+// const students = require('../lib/mongo').students
 
-// Student
-//     .find()
-//     .exec()
-//     .then(console.log)
-//     .catch(console.error)
+const Student = mongolass.model('students', {
+    StudentID: {type: 'Number', required: true},
+    UserName: {type: 'string', required: true},
+    Password: {type: 'string', required: true},
+    Name: {type: 'string', required: true},
+    GroupID: {type: 'Number'},
+    PeoplePreference: [Number],
+    Mark: [Number],
+});
+
+Student
+    .find()
+    .exec()
+    .then(console.log)
+    .catch(console.error)
 
 
 
