e68ae9edb119c86dd3dee4d5c60e5cd5286e0f1b
dotCyan <cyan13614@hotmail.com>
2020-04-07 01:53:17 +0100

    Session test

--- routes/signin.js
+++ routes/signin.js
@@ -7,6 +7,27 @@ const clientModel = require('../models/client');
 const studentModel = require('../models/student');
 const staffModel = require('../models/staff');
 
+router.post('/', function(req, res) {
+    const email = req.fields.email;
+    const pw = req.fields.password;
+
+    Promise.all([
+        staffModel.getStaffByUserName(email),
+    ])
+        .then(function(result) {
+            const staff = result[0];
+
+            if (!staff && pw === staff.password) {
+                req.session.userinfo = staff.UserName;
+                req.session.username = staff.Name;
+                res.redirect('/staff/my_project');
+            }
+            else {
+                res.redirect('/signin');
+                throw new Error('Invalid username or password');
+            }
+        });
+});
 
 router.get('/', function(req, res) {
     if (req.session.userinfo) {
--- routes/staff.js
+++ routes/staff.js
@@ -17,60 +17,48 @@ const staffID = mongoose.Types.ObjectId('5e7a97ab66135760069ca372');
 // getS(studentID)
 // console.log(s[0]);
 
-router.post('/my_project', function(req, res) {
-    const email = req.fields.email;
-    const pw = req.fields.password;
-
-    Promise.all([
-        staffModel.getStaffByUserName(email),
-    ])
-        .then(function(result) {
-            const staff = result[0];
-
-            if (!staff && pw === staff.password) {
-                req.session.userinfo = staff.UserName;
-                req.session.username = staff.Name;
-                res.redirect('/staff/my_project');
-            }
-            else {
-                res.redirect('/signin');
-                throw new Error('Invalid username or password');
-            }
-        });
-});
-
 router.get('/my_project', function(req, res) {
-    Promise.all([
-        staffModel.getStaffByStaffID(staffID),
-        staffModel.getAllocatedTeamByStaffID(staffID),
-    ])
-        .then(function(result) {
-            const maxDisplay = 4;
-            const staff = result[0];
-            const allTeams = result[1];
-            let groupMember = [];
+    if(req.session.userinfo) {
+        Promise.all([
+            //staffModel.getStaffByStaffID(staffID),
+            //staffModel.getAllocatedTeamByStaffID(staffID),
+            staffModel.getStaffByUserName(req.session.userinfo),
+        ])
+            .then(function(result) {
+                const staff = result[0];
 
-            for (let i = 0; i < allTeams.length; i++) {
-                //console.log(allTeams[i]);
-                groupMember[i] = '';
-                const max = (maxDisplay < allTeams[i].StudentID.length)? maxDisplay : allTeams[i].StudentID.length;
-                for (let j = 0; j < max; j++) {
-                    groupMember[i] = groupMember[i] + allTeams[i].StudentID[j].Name;
-                    if (j < max - 1) {
-                        groupMember[i] = groupMember[i] + ', ';
-                    }
-                    else {
-                        if(allTeams[i].StudentID.length > maxDisplay) groupMember[i] = groupMember[i] + '...';
-                    }
-                }
-            }
-            res.render('staff/my_project', {
-                pageTitle: 'My Projects',
-                username: staff.Name,
-                allTeams: allTeams,
-                groupMember: groupMember,
+                Promise.all([staffModel.getAllocatedTeamByStaffID(staff._id)])
+                    .then(function(result) {
+                        const maxDisplay = 4;
+                        const allTeams = result[1];
+                        let groupMember = [];
+                        
+                        for (let i = 0; i < allTeams.length; i++) {
+                            //console.log(allTeams[i]);
+                            groupMember[i] = '';
+                            const max = (maxDisplay < allTeams[i].StudentID.length)? maxDisplay : allTeams[i].StudentID.length;
+                            for (let j = 0; j < max; j++) {
+                                groupMember[i] = groupMember[i] + allTeams[i].StudentID[j].Name;
+                                if (j < max - 1) {
+                                    groupMember[i] = groupMember[i] + ', ';
+                                }
+                                else {
+                                    if(allTeams[i].StudentID.length > maxDisplay) groupMember[i] = groupMember[i] + '...';
+                                }
+                            }
+                        }
+                        res.render('staff/my_project', {
+                            pageTitle: 'My Projects',
+                            username: staff.Name,
+                            allTeams: allTeams,
+                            groupMember: groupMember,
+                        });
+                    });
             });
-        });
+    }
+    else {
+        res.redirect('/signin');
+    }
 });
 // router.get()
 
